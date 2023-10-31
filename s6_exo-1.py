4382248957f3cc004d77c4146042c866381971b6
Draner <135271207+Renaar@users.noreply.github.com>
2023-10-31 16:39:52 +0000

    modified:   s6_exo-1.py

--- s6_exo-1.py
+++ s6_exo-1.py
@@ -3,17 +3,16 @@ import random # Permet de créer un nombre aléatoire
 n = random.randint( 1, 100) # n va stocker un nombre aléatoire entre 1 et 100
 
 nb_joueurs = int(input("Combien de joueurs ? ")) # On demande le nombre de joueurs
-liste_joueurs = []
-winner = ("x" ,101)
+liste_joueurs = [] # Crée la liste qui contiendra les joueurs
+winner = ("x" ,101) # Crée la variable qui contiendra le gagnant
 
-for i in range(nb_joueurs):
-    joueur = (input("Nom du joueur : "), int(input("Quel est ton numéro ?\n")))
+for i in range(nb_joueurs): # On demande aux joueurs leur nom et leur numéro
+    joueur = (input("Nom du joueur : "), int(input("Quel est ton numéro ? "))) # tuple(nom , numéro)
     liste_joueurs.append(joueur)
 
 for i in liste_joueurs:
     diff = abs(n - i[1])
-    if winner = (i[0], diff)
+    if diff < winner[1]:
+        winner = (i[0], diff)
 
-
-
-# print(liste_joueurs)
+print("Le nombre secret est " + str(n) +". Le gagnant est " + winner[0] + ".")
