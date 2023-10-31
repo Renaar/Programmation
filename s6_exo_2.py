e54fe74e9b766d715c73110324f2182d94ae3bd7
Draner <135271207+Renaar@users.noreply.github.com>
2023-10-31 16:58:14 +0000

    new file:   s6_exo_2.py

---
+++ s6_exo_2.py
@@ -0,0 +1,39 @@
+bibliotheque = {
+    "2974BA" : "J.R.R Tolkien : La communauté de l'anneau",
+    "8467CA" : "Georges Orwell : 1984",
+    "3294BB" : "Maurice Leblanc : Arsène Lupin, Gentleman cambrioleur",
+    "6522DA" : "Stephen Hawking : Une brève histoire du temps",
+    "7754AE" : "Stendhal : Le Rouge et le Noir",
+    "6532EE" : "Victor Hugo : Les Misérables",
+    "6439AD" : "Georges Perec : La Disparition",
+    "8232AB" : "Emile Zola : La bête humaine",
+    "9871EA" : "Jean-Jacques Rousseau : Les Confessions",
+    "2334DD" : "Jean-Jacques Rousseau : Discours sur l'origine et les fondements de l'inégalité parmi les hommes",
+    "9838EA" : "J.K. Rowling : Harry Potter à l'école des sorciers",
+    "5535BE" : "J.K. Rowling : Harry Potter et la Chambre des secrets",
+    "2934AD" : "Christopher Paolini : Eragon",
+    "7477ED" : "Antoine de Saint-Exupéry : Le Petit Prince",
+    "7588AA" : "Guy de Maupassant : Bel-ami",
+    "3523EC" : "Schiller : Der Handschuh",
+    "2377CC" : "Goethe : Die Leiden des jungen Werthers",
+    "8499ED" : "Grass : Die Blechtrommel",
+    "8373AA" : "Frisch : Homo faber",
+    "5666BA" : "Dürrenmatt : Die Physiker",
+    "8847CC" : "Brecht : Die Dreigroschenoper",
+    "9956BC" : "Hesse : Der Steppenwolf",
+    "4334AC" : "Alexandre Dumas : Le Comte de Monte-Cristo",
+    "8858CC" : "Virginia Woolf : une chambre à soi",
+    "3734BC" : "Margaret Mitchell : Autant en emporte le vent",
+    "8767AC" : "Boris Vian : L'écume des jours",
+    "3253CC" : "Simone Veil : Une Vie",
+    "7756CA" : "Françoise Sagan : Bonjour Tristesse",
+    "5452AE" : "Simone de Beauvoir : Le deuxième sexe",
+    "8764CE" : "William Shakespeare : Roméo et Juliette",
+    "9878BB" : "Guy de Maupassant : Le Horla",
+    "5469AC" : "Marcel Proust : À la recherche du temps perdu",
+    "8886BC" : "François Rabelais : Gargantua"
+}
+
+while True:
+    input("")
+    
