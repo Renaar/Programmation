e3a3722ec80decdc12740215abd6d042a3cea27b
Draner <135271207+Renaar@users.noreply.github.com>
2023-11-01 08:43:38 +0000

    modified:   s6_exo_2.py

--- s6_exo_2.py
+++ s6_exo_2.py
@@ -34,26 +34,38 @@ bibliotheque = {
     "8886BC" : "François Rabelais : Gargantua"
 }
 
+emprunt = {}
+
 while True:
     print(" ")
     print("=== Bibliothèque ===")
     print(" ")
-    command = input("Que voulez faire ?\n \"afficher\" pour afficher la liste des livres\n \"ajouter\" pour ajouter un livre à la bibliothèque\n \"supprimer\" pour supprimer un livre de la bibliothèque\n \"rechercher\" pour rechercher un mot\n \"fin\" pour terminer la requête\n Commande : ")
+    command = input("Que voulez faire ?\n"
+                    "\"afficher\" pour afficher la liste des livres de la bibliothèque\n"
+                    "\"emprunts\" pour afficher la liste des livres empruntés\n"
+                    "\"ajouter\" pour ajouter un livre à la bibliothèque\n"
+                    "\"supprimer\" pour supprimer un livre à la bibliothèque\n"
+                    "\"rechercher\" pour rechercher un mot dans la bibliothèque\n"
+                    "\"emprunter\" pour emprunter un livre dans la bibliothèque\n"
+                    "\"rendre\" pour rendre un livre emprunté\n"
+                    "\"fin\" pour terminer la requête\n"
+                    "Commande : ")
+    
     if command == "fin":
         break
 
     elif command == "afficher":
         count = 0
         print(" ")
-        print("====================")
+        print(20 * "=")
         print("Contenu de la bibliothèque")
-        print("====================")
+        print(20 * "=")
         print(" ")
         for i in bibliotheque:
             print("Clé : " + str(i))
             print("Titre : " + bibliotheque[i])
             print(" ")
-            print("==========")
+            print(10 * "=")
             print(" ")
             count += 1
         print("Nombre de livres : " + str(count))
@@ -65,3 +77,69 @@ while True:
         bibliotheque.pop("Référence à supprimer ? ")
 
     elif command == "rechercher":
+        recherche = str(input("Quel mot recherchez-vous ? "))
+        print(" ")
+        for i in bibliotheque:
+            if recherche in bibliotheque[i]:
+                print("Référence : " + i)
+                print("Titre : " + bibliotheque[i])
+                print(" ")
+                print(10 * "=")
+    
+    elif command == "emprunter":
+        ref_emprunt = input("Référence du livre à emprunter : ")
+        if ref_emprunt in emprunt:
+            print(" ")
+            print("Livre déjà emprunté.")
+            print(" ")
+            
+        elif ref_emprunt in bibliotheque:
+            emprunteur = (bibliotheque[ref_emprunt] , input("Nom de l'emprunteur : "))
+            emprunt[ref_emprunt] = emprunteur
+            bibliotheque.pop(ref_emprunt)
+            print(" ")
+            print(f"{emprunteur[1]} a emprunté le livre \"{(emprunt[ref_emprunt])[0]}\".")
+            print(" ")
+
+        else:
+            print(" ")
+            print("Cette référence n'existe pas.")
+            print(" ")
+    
+    elif command == "rendre":
+        ref_rendre = input("Référence du livre à rendre : ")
+        if ref_rendre in emprunt:
+            bibliotheque[ref_rendre] = (emprunt[ref_rendre])[0]
+            emprunt.pop(ref_rendre)
+            print(" ")
+            print(f"Le livre \"{bibliotheque[ref_rendre]}\" a été rendu.")
+        
+        elif ref_rendre in bibliotheque:
+            print(" ")
+            print("Ce livre n'a pas été emprunté.")
+            print(" ")
+
+        else:
+            print(" ")
+            print("Le livre n'existe pas.")
+            print(" ")
+
+    elif command == "emprunts":
+        if bool(emprunt) is False:
+            print(" ")
+            print("Aucun livre n'a été emprunté")
+        else:
+            print(" ")
+            print(10 * "=")
+            print("Livres empruntés")
+            print(10 * "=")    
+            for i in emprunt:
+                print(" ")
+                print(f"Référence : {i}\n"
+                      f"Titre : {(emprunt[i])[0]}")
+                print(" ")
+                print(10 * "=")
+
+    elif bool(bibliotheque.get(command)) is False:
+        print(" ")
+        print("Commande incorrecte.")
